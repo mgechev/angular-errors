2d1b985ef3532a07b2d508b37fa8512e804a79c0
mgechev <mgechev@gmail.com>
2020-05-12 15:12:55 -0700

    Add demo with HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouteConfigLoadStart, RouteConfigLoadEnd, RouterEvent } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { Router, RouteConfigLoadStart, RouteConfigLoadEnd, RouterEvent } from '@
 export class AppComponent {
   loading: boolean;
 
-  constructor(router: Router) {
+  constructor(router: Router, private http: HttpClient) {
     this.loading = false;
 
     router.events.subscribe(
