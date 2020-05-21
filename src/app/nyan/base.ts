5e79eee89482f0043ebac3fcb7ea5c268325872e
mgechev <mgechev@gmail.com>
2020-05-20 17:47:07 -0700

    Add example for inheritance error

---
+++ src/app/nyan/base.ts
@@ -0,0 +1,8 @@
+import { Router } from '@angular/router';
+import { Directive } from '@angular/core';
+
+export class Base {
+  constructor(private router: Router) {
+
+  }
+}
--- src/app/nyan/nyan.component.ts
+++ src/app/nyan/nyan.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Base } from './base';
 
 @Component({
   selector: 'app-nyan',
   template: '<img src="/assets/nyan.png">',
   styleUrls: ['./nyan.component.css']
 })
-export class NyanComponent {}
+export class NyanComponent extends Base {}
